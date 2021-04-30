5dfd8a2fef28c49cfd6653096f6f588a7f7f21f1
Heidi Zhang <qingyuanhzhang@gmail.com>
2021-04-29 23:47:36 -0700

    everything before create and read

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
 
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from app.models.book import Book 
+
+    from app.routes import hello_world_bp
+    app.register_blueprint(hello_world_bp)
+    
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,27 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world", __name__)
+
+@hello_world_bp.route('/hello-world', methods=["GET"])
+def get_hello_world():
+    my_reponse = "Hello, World!"
+    return my_reponse
+
+@hello_world_bp.route('/hello-world/JSON', methods=["GET"])
+def say_hello_json():
+    return {
+        "name": "chilicious!",
+        "message": "HELLO!",
+        "hobbies": ["Dancing", "Exploring food", "Coffee"]
+    }, 200
+
+@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
+def broken_endpoint():
+    response_body = {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+    new_hobby = "Surfing"
+    response_body["hobbies"] + [new_hobby] 
+    return response_body
